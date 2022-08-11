54f8e6f1e187e05d24e9dad71a0732d39e1a8315
nilmani2250 <111082370+nilmani2250@users.noreply.github.com>
2022-08-11 23:53:53 +0530

    Sum of digit

---
+++ c.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+/*
+int main() {
+    int n;
+    scanf("%d", &n);
+    //Complete the code to calculate the sum of the five digits on n.
+     int sum = 0;
+     int rem = 0;
+     if(10000 <= n && n <= 99999)
+     {
+         for (; n > 0 ;) {
+             rem = n %10;
+             sum = sum + rem;
+             n = n/10;
+            }
+     }
+     printf ("%d", sum);
+    return 0;
+}
+*/
+
