c16da7a3e8157bde57ed56f544db369f2bf09636
nilmani2250 <111082370+nilmani2250@users.noreply.github.com>
2022-08-13 18:11:41 +0530

    Update palindrome.c

--- palindrome.c
+++ palindrome.c
@@ -1,1 +1,27 @@
-# vector_c
+#include<stdio.h>
+int palindrome_check(int);
+void main()
+{
+  int num,r,sum;
+  printf("Enter the number\n");
+  scanf("%d\n",&num);
+  r=palidrome_check(num);
+  if(r==1)
+    printf("palidrome...\n");
+  else
+    printf("Not palindrome...\n");
+}
+int palindrome_check(int num)
+{
+  int sum=0,temp=num,r;
+  while(temp!=0)
+  {
+    r=temp%10;
+    sum=sum*10+r;
+    temp=temp/10;
+  }
+  if(num==sum)
+    return 1;
+  else
+    return 0;
+}
